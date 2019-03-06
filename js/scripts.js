cd6656713b5aae63e4253993442b593f55f76eed
Dickson <dicksonparlma@gmail.com>
2019-03-06 16:50:57 +0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,35 @@ $(document).ready(function(){
     $("#content2").slideToggle(80);
   });
 });
+$(document).ready(function(){
+  $("#selectForm").submit(function(event){
+    event.preventDefault();
+    var x = $("input[name=a1]:checked").val();
+    var z = $("input[name=a2]:checked").val();
+    var w = $("input[name=a3]:checked").val();
+    var y = $("input[name=a4]:checked").val();
+    var increment = -1;
+    price=0
+
+    items=[x,z,w,y];
+    values=["1","2","3","4"];
+
+    for(i=0;i<=items.length;i++){
+       if(items[i]==values[i]){
+           increment +=1
+           document.getElementById("counter").innerHTML = increment;
+          values.forEach(function(elements){
+          var numbers = parseInt(elements)
+          for(j=0;j<=numbers;j++){
+            finalPrice=parseInt(price=price+numbers)
+            document.getElementById("price").innerHTML = finalPrice;
+          }
+
+        });
+       }
+     }
+});
+});
 var x = document.getElementById("myCheck1").value;
 var z = document.getElementById("myCheck1").value;
 var w = document.getElementById("myCheck1").value;
