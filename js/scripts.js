0a02712b779b59019074a7c0af7372104bd55d9d
Dickson <dicksonparlma@gmail.com>
2019-03-07 12:26:04 +0300

    cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
   $("#food").submit(function(event){
     event.preventDefault();
@@ -15,90 +13,84 @@ $(document).ready(function(){
     var j = $("input[name=Bf10]:checked").val();
     var k = $("input[name=Bf11]:checked").val();
     var l = $("input[name=Bf12]:checked").val();
-    var increment = -1;
+    var increment =0;
     price=0
 
     items=[a,b,c,d,e,f,g,h,i,j,k,l];
-    values=["199","299","300","200","599","499","699","250","999","1099","1200","450"];
-    prices=[199,299,300,200,599,499,699,250,999,1099,1200,450]
+    values=[199,299,300,200,599,499,699,250,999,1099,1200,450];
     totalValues=[];
     totalValues.length=0;
     for(i=0;i<=items.length;i++){
-       if(items[i]==values[i]){
+       if(items[i]===values[i]){
            increment +=1
            document.getElementById("counter").innerHTML = increment;
            t=totalValues.push(parseInt(values));
+           price+=t[i];
 
 
         }
-        for(var j=i;j<=prices.length;j++){
-            prices[j]==items[j]
-            price+=prices
-
-
-
-
         }
-       }
-
-
-
-
-
-       // price+=t;
-       // document.getElementById("price").innerHTML = price;
-
-    //     finalPrice=parseInt(values)
-    //     price+=finalPrice[j];
-    //     document.getElementById("price").innerHTML = price;
-    // }
-
-});
-});
-
-
-
-
-
-$(document).ready(function(){
-  $("#display").click(function(){
-    $(".navbar").slideToggle(800);
-  });
-  $("#morning").click(function(){
-    $("#breakfast").slideDown(800);
-    $("#lunch").slideUp(800);
-    $("#dinner").slideUp(800);
-    $("#startUp").hide(1500);
-  });
-  $("#noon").click(function(){
-    $("#lunch").slideDown(800);
-    $("#breakfast").slideUp(800);
-    $("#dinner").slideUp(800);
-    $("#startUp").hide(800);
-  });
-  $("#night").click(function(){
-    $("#dinner").slideDown(800);
-    $("#breakfast").slideUp(800);
-    $("#lunch").slideUp(800);
-    $("#startUp").hide(800);
-  });
-  $("#orderBreakfast").click(function(){
-    $("#choice1").slideDown(80);
-  });
-  $("#orderLunch").click(function(){
-    $("#choice2").slideDown(80);
-  });
-  $("#orderDinner").click(function(){
-    $("#choice3").slideDown(80);
-  });
-  $("#packageOne").click(function(){
-    $("#content1").slideToggle(80);
-  });
-  $("#packageTwo").click(function(){
-    $("#content2").slideToggle(80);
-  });
-});
-$("#orderBreakfast").click(function(event){
-        event.preventDefault();
-        $("#food").slideToggle(800);
-});
+      });
+    });
+//
+//
+//
+//
+//
+//        // price+=t;
+//        // document.getElementById("price").innerHTML = price;
+//
+//     //     finalPrice=parseInt(values)
+//     //     price+=finalPrice[j];
+//     //     document.getElementById("price").innerHTML = price;
+//     // }
+//
+// });
+// });
+//
+//
+//
+//
+//
+// $(document).ready(function(){
+//   $("#display").click(function(){
+//     $(".navbar").slideToggle(800);
+//   });
+//   $("#morning").click(function(){
+//     $("#breakfast").slideDown(800);
+//     $("#lunch").slideUp(800);
+//     $("#dinner").slideUp(800);
+//     $("#startUp").hide(1500);
+//   });
+//   $("#noon").click(function(){
+//     $("#lunch").slideDown(800);
+//     $("#breakfast").slideUp(800);
+//     $("#dinner").slideUp(800);
+//     $("#startUp").hide(800);
+//   });
+//   $("#night").click(function(){
+//     $("#dinner").slideDown(800);
+//     $("#breakfast").slideUp(800);
+//     $("#lunch").slideUp(800);
+//     $("#startUp").hide(800);
+//   });
+//   $("#orderBreakfast").click(function(){
+//     $("#choice1").slideDown(80);
+//   });
+//   $("#orderLunch").click(function(){
+//     $("#choice2").slideDown(80);
+//   });
+//   $("#orderDinner").click(function(){
+//     $("#choice3").slideDown(80);
+//   });
+//   $("#packageOne").click(function(){
+//     $("#content1").slideToggle(80);
+//   });
+//   $("#packageTwo").click(function(){
+//     $("#content2").slideToggle(80);
+//   });
+// });
+// $("#orderBreakfast").click(function(event){
+//         event.preventDefault();
+//         $("#food").slideToggle(800);
+// });
