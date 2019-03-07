5a9f0200b13325bdfe6a065c3e0e2f8e834ba5b2
AsmaAdan <asmaahmed.adan@gmail.com>
2019-03-07 17:35:49 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 
       }
     }
-    window.location.href = 'mailto:address@dmail.com?subject=Hello there&body=Your bill is '+" "+(price*people)+" "+"for"+" "+delivery;
+    window.location.href = 'mailto: dicksonparlma@gmail.com,asmaahmed.adan@gmail.com, jlmukono@gmail.com ?subject=Please complete your order &body=Your bill is '+" "+(price*people)+" "+"for"+" "+delivery;
 });
 
 });
